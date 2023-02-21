dae9040daffff071422cad4d147015c3ab90f043
Mariia-YMM <119403742+Mariia-YMM@users.noreply.github.com>
2023-02-21 12:12:34 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let first = prompt('Please enter the first number');
+let second = prompt('Please enter the second number');
+alert(`Calculations are finished!
+Sum: ${first} + ${second} = ${+first + +second}
+Diff: ${first} - ${second} = ${first - second} 
+Mult: ${first} * ${second} = ${first * second} 
+Div: ${first} / ${second} = ${first / second}`);
+
