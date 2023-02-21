9260cbc37737da667c2b33f5ff38a41997b389f1
Mariia-YMM <119403742+Mariia-YMM@users.noreply.github.com>
2023-02-21 12:16:44 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-let first = prompt('Please enter the first number');
-let second = prompt('Please enter the second number');
-alert(`Calculations are finished!
-Sum: ${first} + ${second} = ${+first + +second}
-Diff: ${first} - ${second} = ${first - second} 
-Mult: ${first} * ${second} = ${first * second} 
-Div: ${first} / ${second} = ${first / second}`);
-
