6c983ca756a0257566ac15348d639da58c5f6b55
Mariia-YMM <119403742+Mariia-YMM@users.noreply.github.com>
2023-02-21 12:28:59 +0100

    Update script.js

--- HW1/script.js
+++ HW1/script.js
@@ -2,7 +2,7 @@ alert('Hello');
 const name = prompt('What is your name?');
 const age = prompt('What is your birth year?');
 const country = prompt('Where are you from?');
-alert(`Nice to meet you, ${name}!, You are ${2023 - age} years old!, You are from ${country}!`)
+alert(`Nice to meet you, ${name}! You are ${2023 - age} years old! You are from ${country}!`)
 
 
 
