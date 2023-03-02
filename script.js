3af1497c8e91b3369bffddba1dec19125b4c4705
Mariia-YMM <119403742+Mariia-YMM@users.noreply.github.com>
2023-03-02 12:41:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,39 +2,39 @@ const typeMathoperation = prompt('Please select the type of mathematical operati
 let firstNumber = 'Please enter the first number';
 let secondNumber = 'Please enter the second number';
 switch (typeMathoperation){
-    case 'sum':{
+    case 'sum':
         let first = prompt(firstNumber);
         let second = prompt(secondNumber);
         alert( `Calculations are finished! Sum: ${first} + ${second} = ${+first + +second}`);
-        break};
-    case 'diff':{
+        break;
+    case 'diff':
         let first = prompt(firstNumber);
         let second = prompt(secondNumber);
         alert( `Calculations are finished! Diff: ${first} - ${second} = ${first - second}`);
-        break};
-    case 'mult':{
+        break;
+    case 'mult':
         let first = prompt(firstNumber);
         let second = prompt(secondNumber);
         alert( `Calculations are finished! Mult: ${first} * ${second} = ${first * second}`);
-        break};
-    case 'div':{
+        break;
+    case 'div':
         let first = prompt(firstNumber);
         let second = prompt(secondNumber);
         alert( `Calculations are finished! Div: ${first} / ${second} = ${first / second}`);
-        break};
-    case 'pow':{
+        break;
+    case 'pow':
         let first = prompt('Please enter number is the base');
         let second = prompt('Please enter number is the exponent');
         alert(Math.pow(first, second));
-        break};
-    case 'sin':{
+        break;
+    case 'sin':
         let first = prompt(firstNumber);
         alert(Math.sin(first));
-        break};
-    case 'cos':{
+        break;
+    case 'cos':
         let first = prompt(firstNumber);
         alert(Math.cos(first));
-        break};
+        break;
 }
 
 
