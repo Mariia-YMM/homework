b229216cdc4178e65cebe10ec365d0ac2918a1a2
Mariia-YMM <119403742+Mariia-YMM@users.noreply.github.com>
2023-03-02 11:46:42 +0100

    Add files via upload

---
+++ HW3/script.js
@@ -0,0 +1,31 @@
+const typeMathoperation = prompt('Please select the type of mathematical operation: sum, diff, mult, div, pow, sin, cos');
+let firstNumber = 'Please enter the first number';
+let secondNumber = 'Please enter the second number';
+if (typeMathoperation == 'sum'){
+    let first = prompt(firstNumber);
+    let second = prompt(secondNumber);
+    alert( `Calculations are finished! Sum: ${first} + ${second} = ${+first + +second}`);
+} else if (typeMathoperation == 'diff'){
+    prompt(first);
+    prompt(second);
+    alert( `Calculations are finished! Diff: ${first} - ${second} = ${first - second}`);
+} else if (typeMathoperation == 'mult'){
+    let first = prompt('Please enter the first number');
+    let second = prompt('Please enter the second number');
+    alert( `Calculations are finished! Mult: ${first} * ${second} = ${first * second}`);
+} else if (typeMathoperation == 'div'){
+    let first = prompt('Please enter the first number');
+    let second = prompt('Please enter the second number');
+    alert( `Calculations are finished! Div: ${first} / ${second} = ${first / second}`);
+} else if (typeMathoperation == 'pow'){
+    let first = prompt('Please enter number is the base');
+    let second = prompt('Please enter number is the exponent');
+    alert(Math.pow(first, second));
+} else if (typeMathoperation == 'sin'){
+    let first = prompt('Please enter number');
+    alert(Math.sin(first));
+} else if (typeMathoperation == 'cos'){
+    let first = prompt('Please enter number');
+    alert(Math.cos(first));
+}
+
