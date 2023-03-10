c350865d557255f389a4f7bb03844ed491ad1056
Mariia-YMM <119403742+Mariia-YMM@users.noreply.github.com>
2023-03-10 15:38:25 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+let history = [];
+let operation, num1, num2, result;
+do {
+    operation = prompt('Please select the type of mathematical operation: add, sub, mult, div, sin, cos, pow');
+} while (!['add', 'sub', 'mult', 'div', 'sin', 'cos', 'pow'].includes(operation));
+
+do {
+    num1 = parseFloat(prompt('Please enter the first number'));
+} while (isNaN(num1));
+
+if (operation === 'sin' || operation === 'cos') {
+    result = Math[operation](num1);
+} else {
+    do {
+        num2 = parseFloat(prompt('Please enter the second number'));
+    } while (isNaN(num2));
+
+    if (operation === 'add') {
+        result = num1 + num2;
+    } else if (operation === 'sub') {
+        result = num1 - num2;
+    } else if (operation === 'mult') {
+        result = num1 * num2;
+    } else if (operation === 'div') {
+        result = num1 / num2;
+    } else if (operation === 'pow') {
+        result = Math.pow(num1, num2);
+    }
+}
+
+console.log("Operation " + operation + " finished with result " + result);
+history.push("Operation " + operation + " finished with result " + result);
+
+while (confirm('Do you want to perform another operation?')) {
+    do {
+        operation = prompt('Please select the type of mathematical operation: add, sub, mult, div, sin, cos, pow');
+    } while (!['add', 'sub', 'mult', 'div', 'sin', 'cos', 'pow'].includes(operation));
+
+    do {
+        num1 = parseFloat(prompt('Please enter the first number'));
+    } while (isNaN(num1));
+
+    if (operation === 'sin' || operation === 'cos') {
+        result = Math[operation](num1);
+    } else {
+        do {
+            num2 = parseFloat(prompt('Please enter the second number'));
+        } while (isNaN(num2));
+
+        if (operation === 'add') {
+            result = num1 + num2;
+        } else if (operation === 'sub') {
+            result = num1 - num2;
+        } else if (operation === 'mult') {
+            result = num1 * num2;
+        } else if (operation === 'div') {
+            result = num1 / num2;
+        } else if (operation === 'pow') {
+            result = Math.pow(num1, num2);
+        }
+    }
+
+    console.log("Operation " + operation + " finished with result " + result);
+    history.push("Operation " + operation + " finished with result " + result);
+}
+if (confirm('Want to see your transaction history?')) {
+    console.log(history.join('\n'));
+}
